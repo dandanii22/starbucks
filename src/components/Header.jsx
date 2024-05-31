1787d52230c2a61ada02c2d1f0e9457397866797
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-05-31 15:26:12 +0900

    우유림 헤더,푸터 작업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+import { HeaderWarp } from './HeaderStyle';
+import NavBar from './NavBar';
 const Header = () => {
-  return <div></div>;
+    return (
+        <HeaderWarp>
+            <div className="inner">
+                <h1>
+                    <Link to={'/'}>
+                        <img src="" alt="스타벅스 로고" />
+                    </Link>
+                </h1>
+                <NavBar />
+            </div>
+        </HeaderWarp>
+    );
 };
 
 export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Footer from "./Footer";
-import Header from "./Header";
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
+import Header from './Header';
 
 const Layout = () => {
-  return (
-    <div className="wrap">
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="wrap">
+            <Header />
+            <div className="main">
+                <Outlet />
+            </div>
+
+            <Footer />
+        </div>
+    );
 };
 
 export default Layout;
