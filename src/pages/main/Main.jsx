f6faf0f5c7a35914a42c6d57c7908b05bbeb794f
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-06-03 10:14:56 +0900

    Add files via upload

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,5 +1,28 @@
+import MainVisual from '../../components/Main/MainVisual';
+import Maincon1 from '../../components/Main/Maincon1';
+import Maincon2 from '../../components/Main/Maincon2';
+import Maincon3 from '../../components/Main/Maincon3';
+import Maincon4 from '../../components/Main/Maincon4';
+import Maincon5 from '../../components/Main/Maincon5';
+import Maincon6 from '../../components/Main/Maincon6';
+import { MainWrap } from './MainStyle';
+
 const Main = () => {
-  return <div>Main</div>;
+    return (
+        <MainWrap>
+            <MainVisual />
+            <Maincon1 />
+            <Maincon2 />
+            <Maincon3 />
+            <div className="inner">
+                <img className="subimg" src="./images/main/bg.png" alt="" />
+            </div>
+
+            <Maincon4 />
+            <Maincon5 />
+            <Maincon6 />
+        </MainWrap>
+    );
 };
 
 export default Main;
--- src/pages/main/MainStyle.js
+++ src/pages/main/MainStyle.js
@@ -1,3 +1,18 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const MainWrap = styled.div``;
+export const MainWrap = styled.main`
+    width: 100%;
+    position: relative;
+
+    .inner {
+        width: 1520px;
+        img.subimg {
+            width: 1920px;
+            opacity: 0.5;
+            position: absolute;
+            left: 50%;
+            height: 1800px;
+            transform: translateX(-50%);
+        }
+    }
+`;
