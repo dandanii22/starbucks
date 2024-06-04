e87d210526e9d493a9ff7eaf003912c3bc45fbbd
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-06-04 09:39:05 +0900

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../store/modules/authSlice';
 
 export const store = configureStore({
-  reducer: {},
+    reducer: {
+        authR: authReducer,
+    },
 });
