a5fbd4f90489e2663306d6f3d0e546268c5fe87b
kkyoung635 <158258174+kkyoung635@users.noreply.github.com>
2024-06-04 14:27:12 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../store/modules/authSlice';
+import myStar from './modules/myStarbucksSlice';
+
 
 export const store = configureStore({
     reducer: {
