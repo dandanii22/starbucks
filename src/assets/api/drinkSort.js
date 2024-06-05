93fc4f7073d0be611885090689627af112739d2d
pui429 <168400717+pui429@users.noreply.github.com>
2024-06-05 13:50:14 +0900

    Add files via upload

---
+++ src/assets/api/drinkSort.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    drinkSort: "콜드 브루 커피",
+    engName: "coldbrew",
+    isShow: false,
+  },
+  { id: 2, drinkSort: "브루드 커피", engName: "brewed", isShow: false },
+  { id: 3, drinkSort: "에스프레소", engName: "espresso", isShow: false },
+  { id: 4, drinkSort: "프라푸치노", engName: "frappuccino", isShow: false },
+  { id: 5, drinkSort: "블렌디드", engName: "blended", isShow: false },
+  {
+    id: 6,
+    drinkSort: "스타벅스 리프레셔",
+    engName: "refreshers",
+    isShow: false,
+  },
+  { id: 7, drinkSort: "스타벅스 피지오", engName: "fizzio", isShow: false },
+  { id: 8, drinkSort: "티(티바나)", engName: "tea", isShow: false },
+  { id: 9, drinkSort: "기타 제조 음료", engName: "etc", isShow: false },
+  {
+    id: 10,
+    drinkSort: "스타벅스 주스(병음료)",
+    engName: "bottle",
+    isShow: false,
+  },
+];
