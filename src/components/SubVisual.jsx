f44958e4e00159c5698997a29f5e899b052de3ca
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-06-07 16:52:32 +0900

    Add files via upload

---
+++ src/components/SubVisual.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const SubVisualWrap = styled.section``;
+
+const SubVisual = () => {
+  return (
+    <SubVisualWrap bgimg="">
+      <h2>xxx 페이지영역입니다. </h2>
+    </SubVisualWrap>
+  );
+};
+
+export default SubVisual;
