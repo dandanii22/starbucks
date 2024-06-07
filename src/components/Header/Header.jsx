cc6aa5b80a7a86dff195b583b4c846f2bb8b200d
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-06-07 15:15:04 +0900

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,27 @@
-import { Link } from "react-router-dom";
-import { HeaderWarp } from "./HeaderStyle";
-import NavBar from "./NavBar";
+import { Link } from 'react-router-dom';
+import { HeaderWarp } from './HeaderStyle';
+import NavBar from './NavBar';
+import styled from 'styled-components';
 
+const Hwrap = styled.div`
+    width: 100%;
+    height: 120px;
+`;
 const Header = () => {
-  return (
-    <HeaderWarp>
-      <div className="inner">
-        <h1>
-          <Link to={"/"}>
-            <img src="../../../public/images/logo.png" alt="스타벅스 로고" />
-          </Link>
-        </h1>
-        <NavBar />
-      </div>
-    </HeaderWarp>
-  );
+    return (
+        <Hwrap>
+            <HeaderWarp>
+                <div className="inner">
+                    <h1>
+                        <Link to={'/'}>
+                            <img src="./images/logo.png" alt="스타벅스 로고" />
+                        </Link>
+                    </h1>
+                    <NavBar />
+                </div>
+            </HeaderWarp>
+        </Hwrap>
+    );
 };
 
 export default Header;
--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,147 +1,149 @@
-import { Link } from "react-router-dom";
-import { NavWarp, TopMenu } from "./HeaderStyle";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+import { NavWarp, TopMenu } from './HeaderStyle';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const NavBar = () => {
-  const [coffeeOpen, setCoffeeOpen] = useState(false);
-  const [foodOpen, setFoodOpen] = useState(false);
+    const [coffeeOpen, setCoffeeOpen] = useState(false);
+    const [foodOpen, setFoodOpen] = useState(false);
 
-  const [productOpen, setProductOpen] = useState(false);
+    const [productOpen, setProductOpen] = useState(false);
 
-  const handleMouseEnter1 = () => setCoffeeOpen(true);
-  const handleMouseLeave1 = () => setCoffeeOpen(false);
+    const handleMouseEnter1 = () => setCoffeeOpen(true);
+    const handleMouseLeave1 = () => setCoffeeOpen(false);
 
-  const handleMouseEnter2 = () => setFoodOpen(true);
-  const handleMouseLeave2 = () => setFoodOpen(false);
+    const handleMouseEnter2 = () => setFoodOpen(true);
+    const handleMouseLeave2 = () => setFoodOpen(false);
 
-  const handleMouseEnter3 = () => setProductOpen(true);
-  const handleMouseLeave3 = () => setProductOpen(false);
+    const handleMouseEnter3 = () => setProductOpen(true);
+    const handleMouseLeave3 = () => setProductOpen(false);
 
-  const { authed } = useSelector((state) => state.authR);
-  return (
-    <>
-      <NavWarp>
-        <ul className="gnb">
-          <li
-            onMouseEnter={handleMouseEnter1}
-            onMouseLeave={handleMouseLeave1}
-            className={coffeeOpen ? "on" : ""}
-          >
-            <Link to="/drinkMenu" className="active ">
-              COFFEE
-            </Link>
-          </li>
-          {coffeeOpen && (
-            <div
-              className="dropdown"
-              onMouseEnter={handleMouseEnter1}
-              onMouseLeave={handleMouseLeave1}
-            >
-              <ul>
-                <li className="coffee">
-                  <Link to="/drinkMenu">음료</Link>
-                  <Link to="/drinkMenu">콜드브루</Link>
-                  <Link to="/drinkMenu">브루드커피</Link>
-                  <Link to="/drinkMenu">에스프레소</Link>
-                  <Link to="/drinkMenu">프라푸치노</Link>
-                  <Link to="/drinkMenu">블렌디드</Link>
-                  <Link to="/drinkMenu">스타벅스 리프레셔</Link>
-                  <Link to="/drinkMenu">스타벅스 피지오</Link>
-                  <Link to="/drinkMenu">티(티바나)</Link>
-                  <Link to="/drinkMenu">기타 제조 음료</Link>
-                  <Link to="/drinkMenu">스바벅스 주스(병음료)</Link>
-                </li>
-              </ul>
-            </div>
-          )}
+    const { authed } = useSelector((state) => state.authR);
+    return (
+        <>
+            <NavWarp>
+                <ul className="gnb">
+                    <li
+                        onMouseEnter={handleMouseEnter1}
+                        onMouseLeave={handleMouseLeave1}
+                        className={coffeeOpen ? 'on' : ''}
+                    >
+                        <Link to="coffee" className="active ">
+                            COFFEE
+                        </Link>
+                    </li>
+                    {coffeeOpen && (
+                        <div
+                            className="dropdown"
+                            onMouseEnter={handleMouseEnter1}
+                            onMouseLeave={handleMouseLeave1}
+                        >
+                            <ul>
+                                <li className="coffee">
+                                    <Link to="/menu/coffee">음료</Link>
+                                    <Link to="/menu/coffee">콜드브루</Link>
+                                    <Link to="/menu/coffee">브루드커피</Link>
+                                    <Link to="/menu/coffee">에스프레소</Link>
+                                    <Link to="/menu/coffee">프라푸치노</Link>
+                                    <Link to="/menu/coffee">블렌디드</Link>
+                                    <Link to="/menu/coffee">스타벅스 리프레셔</Link>
+                                    <Link to="/menu/coffee">스타벅스 피지오</Link>
+                                    <Link to="/menu/coffee">티(티바나)</Link>
+                                    <Link to="/menu/coffee">기타 제조 음료</Link>
+                                    <Link to="/menu/coffee">스바벅스 주스(병음료)</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
 
-          <li
-            onMouseEnter={handleMouseEnter2}
-            onMouseLeave={handleMouseLeave2}
-            className={foodOpen ? "on" : ""}
-          >
-            <Link to="/food" className="active">
-              FOOD
-            </Link>
-          </li>
-          {foodOpen && (
-            <div
-              className="dropdown"
-              onMouseEnter={handleMouseEnter2}
-              onMouseLeave={handleMouseLeave2}
-            >
-              <ul>
-                <li className="food">
-                  <Link to="/food">푸드</Link>
-                  <Link to="/food">브레드</Link>
-                  <Link to="/food">케이크</Link>
-                  <Link to="/food">샌드위치 & 샐러드</Link>
-                  <Link to="/food">따뜻한 푸드</Link>
-                  <Link to="/food">과일 & 요거트</Link>
-                  <Link to="/food">스낵 & 미니 디저트</Link>
-                </li>
-              </ul>
-            </div>
-          )}
-          <li
-            onMouseEnter={handleMouseEnter3}
-            onMouseLeave={handleMouseLeave3}
-            className={productOpen ? "on" : ""}
-          >
-            <Link to="product" className="active">
-              PRODUCT
-            </Link>
-          </li>
-          {productOpen && (
-            <div
-              className="dropdown"
-              onMouseEnter={handleMouseEnter3}
-              onMouseLeave={handleMouseLeave3}
-            >
-              <ul>
-                <li className="product">
-                  <Link to="/product">상품</Link>
-                  <Link to="/product">머그</Link>
-                  <Link to="/product">글라스</Link>
-                  <Link to="/product">플라스틱 텀블러</Link>
-                  <Link to="/product">스테인리스 텀블러</Link>
-                  <Link to="/product">보온병</Link>
-                  <Link to="/product">액세서리</Link>
-                  <Link to="/product">선물세트</Link>
-                  <Link to="/product">패키지(티바나)</Link>
-                </li>
-              </ul>
-            </div>
-          )}
-          <li>
-            <Link to="mystarbucks">MY STARBUCKS</Link>
-          </li>
-          <li>
-            <Link to="mycart">
-              MY CART
-              <span>0</span>
-            </Link>
-          </li>
-        </ul>
-        <TopMenu>
-          {authed ? (
-            <li>
-              <Link to={"/logout"}>Logout</Link>
-            </li>
-          ) : (
-            <li>
-              <Link to={"/login"}>Login</Link>
-            </li>
-          )}
+                    <li
+                        onMouseEnter={handleMouseEnter2}
+                        onMouseLeave={handleMouseLeave2}
+                        className={foodOpen ? 'on' : ''}
+                    >
+                        <Link to="/food" className="active">
+                            FOOD
+                        </Link>
+                    </li>
+                    {foodOpen && (
+                        <div
+                            className="dropdown"
+                            onMouseEnter={handleMouseEnter2}
+                            onMouseLeave={handleMouseLeave2}
+                        >
+                            <ul>
+                                <li className="food">
+                                    <Link to="/food">푸드</Link>
+                                    <Link to="/food">브레드</Link>
+                                    <Link to="/food">케이크</Link>
+                                    <Link to="/food">샌드위치 & 샐러드</Link>
+                                    <Link to="/food">따뜻한 푸드</Link>
+                                    <Link to="/food">과일 & 요거트</Link>
+                                    <Link to="/food">스낵 & 미니 디저트</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                    <li
+                        onMouseEnter={handleMouseEnter3}
+                        onMouseLeave={handleMouseLeave3}
+                        className={productOpen ? 'on' : ''}
+                    >
+                        <Link to="product" className="active">
+                            PRODUCT
+                        </Link>
+                    </li>
+                    {productOpen && (
+                        <div
+                            className="dropdown"
+                            onMouseEnter={handleMouseEnter3}
+                            onMouseLeave={handleMouseLeave3}
+                        >
+                            <ul>
+                                <li className="product">
+                                    <Link to="/product">상품</Link>
+                                    <Link to="/product">머그</Link>
+                                    <Link to="/product">글라스</Link>
+                                    <Link to="/product">플라스틱 텀블러</Link>
+                                    <Link to="/product">스테인리스 텀블러</Link>
+                                    <Link to="/product">보온병</Link>
+                                    <Link to="/product">액세서리</Link>
+                                    <Link to="/product">선물세트</Link>
+                                    <Link to="/product">패키지(티바나)</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                    <li>
+                        {authed ? (
+                            <Link to="MyStabucks">MY STARBUCKS</Link>
+                        ) : (
+                            <Link to="before">MY STARBUCKS</Link>
+                        )}
+                    </li>
+                    <li>
+                        <Link to="mycart">
+                            MY CART
+                            <span>0</span>
+                        </Link>
+                    </li>
+                </ul>
+                <TopMenu>
+                    {authed ? (
+                        <li>
+                            <Link to={'/logout'}>Logout</Link>
+                        </li>
+                    ) : (
+                        <li>
+                            <Link to={'/login'}>Login</Link>
+                        </li>
+                    )}
 
-          <li>
-            <Link to={"/join"}>Join us</Link>
-          </li>
-        </TopMenu>
-      </NavWarp>
-    </>
-  );
+                    <li>{authed === false && <Link to={'/join'}>Join us</Link>}</li>
+                </TopMenu>
+            </NavWarp>
+        </>
+    );
 };
 
 export default NavBar;
