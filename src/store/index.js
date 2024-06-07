04ee8386615131d8f264abe9d0bc5b8be584b4b1
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-06-07 15:23:43 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../store/modules/authSlice';
 import myStar from './modules/myStarbucksSlice';
 import drinkMenu from "./modules/drinkMenuSlice";
-
+import noticeR from './modules/noticeSlice';
 
 export const store = configureStore({
     reducer: {
         authR: authReducer,
         drinkMenu,
+         noticeR,
+          myStar,
     },
 });
