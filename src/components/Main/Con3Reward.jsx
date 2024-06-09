696378231deb4d2959e030aa510c5d8543f1d72c
kkyoung635 <158258174+kkyoung635@users.noreply.github.com>
2024-06-09 13:47:41 +0900

    메인 유저리워드 데이터 출력 수정

--- src/components/Main/Con3Reward.jsx
+++ src/components/Main/Con3Reward.jsx
@@ -1,23 +1,48 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { rewards } from '../../store/modules/myStarbucksSlice';
 
 const Con3Reward = () => {
+    const dispatch = useDispatch();
     const { user } = useSelector((state) => state.authR);
+    const { level } = useSelector((state) => state.myStar);
+
+    const {
+        nickName,
+        myReward: { stars },
+    } = user;
+
+    useEffect(() => {
+        dispatch(rewards({ stars }));
+    }, [stars]);
+
     const navigate = useNavigate();
     return (
         <div className="Member">
             <div className="reward-icon">
                 <div className="reward-icon star">
                     <img src="./images/main/star_bg.png" alt="starbucks reward" />
-                    <span className="level">{user.myReward.stars}</span>
+                    <span className="level">{stars}</span>
                 </div>
             </div>
             <div className="reward-text">
                 <h4>
-                    <span>{user.nickName}</span>님의 현재 <span>Welcome Level</span>이십니다.
+                    <span>{nickName}</span>님의 현재{' '}
+                    <b className={`txt-bold ${level === 'Gold' ? 'gold' : level === 'Green' ? 'green' : 'gray'}`}>{level} Level </b>이십니다.
                 </h4>
                 <p>
-                    <span className="color">Green Level</span>이 되기까지 5개의 별이 필요합니다.
+                    {level === 'Gold' ? (
+                        <span></span>
+                    ) : level === 'Green' ? (
+                        <span>
+                            <em className="txt-bold gold"> Gold level</em>이 되기까지 {30 - stars}의 별이 필요합니다.
+                        </span>
+                    ) : (
+                        <span>
+                            <em className="txt-bold green"> Green level</em>이 되기까지 {5 - stars}의 별이 필요합니다.
+                        </span>
+                    )}
                 </p>
 
                 <p className="btn">
