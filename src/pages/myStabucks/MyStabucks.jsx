1f90963e59c3e5f6ef411e033420641ca5bd71f1
kkyoung635 <158258174+kkyoung635@users.noreply.github.com>
2024-06-09 13:45:52 +0900

    로컬스토리지 유저 비동기 데이터 수정

--- src/pages/myStabucks/MyStabucks.jsx
+++ src/pages/myStabucks/MyStabucks.jsx
@@ -3,7 +3,20 @@ import MyFrequency from '../../components/myStabucks/MyFrequency';
 import MyMenu from '../../components/myStabucks/MyMenu';
 import MyReward from '../../components/myStabucks/MyReward';
 import { MyStabucksWrap } from './MyStabucksStyle.js';
+
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../store/modules/myStarbucksSlice.js';
+
 const MyStabucks = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const localUserData = localStorage.getItem('user');
+        if (localUserData) {
+            dispatch(setUser(JSON.parse(localUserData))); // 로컬 스토리지에서 사용자 정보를 읽어와 스토어에 설정
+        }
+    }, [dispatch]);
+
     return (
         <>
             <MyStabucksWrap className="page-mypage">
