5100eaf1ab1b8366bde08f6f7b8e15c0b65c30f0
kkyoung635 <158258174+kkyoung635@users.noreply.github.com>
2024-06-10 18:15:24 +0900

    useEffect 누락 건 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LoginWrap } from './LoginStyle';
 import { FaCheck } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
