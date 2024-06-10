5f0121e136b99d498cafbdaf18e30cc8cd92d85d
Wooyoulim <168395738+Wooyoulim@users.noreply.github.com>
2024-06-10 11:56:31 +0900

    로그인 하기 전 버튼 기능 추가

--- src/pages/login/BeforeLogin.jsx
+++ src/pages/login/BeforeLogin.jsx
@@ -1,6 +1,8 @@
 import { LoginBefore } from './LoginStyle';
+import { useNavigate } from 'react-router-dom';
 
 const BeforeLogin = () => {
+        const navigate = useNavigate();
     return (
         <LoginBefore>
             <img src="./images/login/beforeLogin.png" alt="로그인이 필요합니다" />
@@ -10,8 +12,12 @@ const BeforeLogin = () => {
                 </h3>
                 <p>해당 서비스를 이용하시려면 스타벅스 회원으로 가입하시기 바랍니다.</p>
                 <p>
-                    <button className="btn1">로그인</button>
-                    <button className="btn2">회원가입</button>
+               <button className="btn1" onClick={() => navigate('/login')}>
+                        로그인
+                    </button>
+                    <button className="btn2" onClick={() => navigate('/join')}>
+                        회원가입
+                    </button>
                 </p>
             </div>
         </LoginBefore>
