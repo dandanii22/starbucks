bf1c0a41195bd0dea48a5f573fac905efde7c117
kkyoung635 <158258174+kkyoung635@users.noreply.github.com>
2024-06-11 16:33:02 +0900

    Add files via upload

--- src/pages/myStabucks/MyStabucks.jsx
+++ src/pages/myStabucks/MyStabucks.jsx
@@ -5,8 +5,24 @@ import MyReward from '../../components/myStabucks/MyReward';
 import { MyStabucksWrap } from './MyStabucksStyle.js';
 
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../store/modules/myStarbucksSlice.js';
 
 const MyStabucks = () => {
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const localUserData = localStorage.getItem('user');
+        if (localUserData) {
+            dispatch(setUser(JSON.parse(localUserData))); // 사용자 정보 스토어에 설정
+        }
+        setLoading(false);
+    }, [dispatch]);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
     return (
         <>
             <MyStabucksWrap className="page-mypage">
