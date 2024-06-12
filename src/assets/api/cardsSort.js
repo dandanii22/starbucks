c5eec59f7e3c1294c423e5566b1f4f210121c966
pui429 <168400717+pui429@users.noreply.github.com>
2024-06-12 11:50:11 +0900

    Add files via upload

---
+++ src/assets/api/cardsSort.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    engName: "real",
+    kor: "실물카드",
+    isShow: false,
+  },
+  {
+    id: 2,
+    engName: "mobile",
+    kor: "스타벅스 모바일 카드",
+    isShow: false,
+  },
+];
---
+++ src/assets/api/cardsThemaSort.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    engName: "birth",
+    kor: "축하",
+    imgurl: "../../public/images/cards/card_bn01.png",
+  },
+  {
+    id: 2,
+    engName: "cheers",
+    kor: "응원",
+    imgurl: "../../public/images/cards/card_bn02.png",
+  },
+  {
+    id: 3,
+    engName: "thanks",
+    kor: "감사",
+    imgurl: "../../public/images/cards/card_bn03.png",
+  },
+  {
+    id: 4,
+    engName: "coffee",
+    kor: "Coffe Favorite",
+    imgurl: "../../public/images/cards/card_bn04.png",
+  },
+  {
+    id: 5,
+    engName: "love",
+    kor: "사랑",
+    imgurl: "../../public/images/cards/card_bn05.png",
+  },
+  {
+    id: 6,
+    engName: "season",
+    kor: "시즌",
+    imgurl: "../../public/images/cards/card_bn06.png",
+  },
+];
